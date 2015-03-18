5b8ff07989f04bfdb9438685904f7c2901923083
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-03-18 02:18:23 -0500

    fix launch issue

--- portfolio/app.py
+++ portfolio/app.py
@@ -1,6 +1,5 @@
 __author__ = 'Xuefeng Zhu'
 
-import os
 from flask import Flask
 from flask.ext.assets import Environment
 from portfolio.assets import bundles
@@ -8,18 +7,14 @@ import portfolio.utils.parser as parser
 from portfolio.controllers.project_view import project
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config["project_dict"] = parser.parse_svn_list()
-    app.config["log_dict"] = parser.parse_svn_log()
+app = Flask(__name__)
+app.config["project_dict"] = parser.parse_svn_list()
+app.config["log_dict"] = parser.parse_svn_log()
 
-    assets = Environment(app)
-    assets.register(bundles)
+assets = Environment(app)
+assets.register(bundles)
 
-    app.register_blueprint(project)
+app.register_blueprint(project)
 
-    return app
-
-
-app = create_app()
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
